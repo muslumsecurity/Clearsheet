1b45c8ddd14a3891077e2e8d93be51a02e9f0359
Muslum DAG <48261594+muslumhack@users.noreply.github.com>
2023-04-22 19:23:12 +0300

    Create veritipleri.php

---
+++ php/veritipleri.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$new_string = "muslum"; // string
+$new_int = 5; // tam sayi integer
+$new_float = 5.5 ;// reel sayi float veya double 
+$new_boolean = true ; //mantıksal doğru - boolean
+$new_boolean2 = false; // mantıksal yanlış boolean
+
+
+
+
+?>
