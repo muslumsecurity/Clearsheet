0785dc7f7958633daf6140dd18365d0620dcf00e
Muslum DAG <48261594+muslumhack@users.noreply.github.com>
2023-04-22 19:32:26 +0300

    Create dizilerornek.php

---
+++ php/dizilerornek.php
@@ -0,0 +1,24 @@
+<?php
+                0       1       2
+$new_dizi = ["opel","renault","WV"];
+echo $new_dizi[0];
+echo $new_dizi[1];
+echo $new_dizi[2];
+
+$new_dizi2 = ["opel",1.2,98];
+echo $new_dizi2[0];
+echo $new_dizi2[1];
+echo $new_dizi3[2];
+
+
+$new_cars = ["Marka" => "Opel",
+             "Hacim" => 1.8,
+             "Guc" => 85];
+             
+echo $new_cars["Marka"];
+echo $new_cars["Hacim"];
+echo $new_cars["Guc"];
+
+
+
+?>
