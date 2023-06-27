1df454630ec97dd2a30953080710db15b3a2c066
Muslum DAG <48261594+muslumhack@users.noreply.github.com>
2023-06-27 18:45:11 +0300

    Create htmlinjection.php

---
+++ php/security/htmlinjection.php
@@ -0,0 +1,28 @@
+
+<html>
+<head></head>
+  <body>
+
+    <center>
+    <h1> YORUM KUTUSU </h1>
+    <form action="" method="get">
+      <textarea cols='40' rows='4'></textarea>
+      <input type="submit" value="paylas" name="share"/>
+    </form>
+
+    <h1> YORUMLAR <h1>
+    <?php 
+
+
+
+
+
+    ?>
+
+  </center>  
+
+
+
+    
+  </body>
+</html>
