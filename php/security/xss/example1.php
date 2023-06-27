9831dc93147ebe45422076822f6e08f2c764301f
Muslum DAG <48261594+muslumhack@users.noreply.github.com>
2023-06-27 17:56:59 +0300

    Update example1.php

--- php/security/xss/example1.php
+++ php/security/xss/example1.php
@@ -1,1 +1,37 @@
+<html>
+  <head></head>
+  <body>
 
+  <form action="" method="post">
+    <input type="text" name="data">
+    <input type="submit" value="gonder" name="button">
+  </form>
+
+
+
+  <?php
+
+  function ekranabastir($vlaue){
+      echo $value;
+  }
+
+
+
+  if(!isset($_POST['button'])){
+    echo 'form boş';
+  }else{
+    if(empty($data)){
+      echo 'veri yok';
+    }else{
+      $value = $_POST['data'];
+      ekranabastir($value);
+    }
+  }
+
+
+
+  ?>
+
+    
+  </body>
+</html>
