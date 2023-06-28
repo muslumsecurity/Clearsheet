cb617bd7ef21af45daa6150630e5a52d1393a657
Muslum DAG <48261594+muslumhack@users.noreply.github.com>
2023-06-28 18:15:51 +0300

    Create Verieklemeornek.php

---
+++ php/databaseislemleri/Verieklemeornek.php
@@ -0,0 +1,22 @@
+1. Veri Ekleme (Insert):
+
+<!-- HTML -->
+<form method="POST">
+    İsim: <input type="text" name="name"><br>
+    Email: <input type="text" name="email"><br>
+    <button type="submit" name="ekle">Ekle</button>
+</form>
+
+<!-- PHP -->
+<?php
+$pdo = new PDO('mysql:host=localhost;dbname=test', 'root', '');
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['ekle'])) {
+    $sql = "INSERT INTO my_table (name, email) VALUES (?, ?)";
+    $stmt= $pdo->prepare($sql);
+    $stmt->execute([$_POST['name'], $_POST['email']]);
+}
+?>
+
+
+
+
