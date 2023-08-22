b1bad03c00701c5b71f8a62551d134e9538d7ca2
Muslum DAG <48261594+muslumhack@users.noreply.github.com>
2023-08-22 16:21:09 +0300

    Create ifelse.php

---
+++ php/ifelse.php
@@ -0,0 +1,55 @@
+#isset kullanımı
+if (isset($variable)) {
+    // $variable tanımlı ise burası çalışır
+} else {
+    // $variable tanımlı değilse burası çalışır
+}
+
+--------------------------------
+#empty
+if (empty($variable)) {
+    // $variable boş ise burası çalışır
+} else {
+    // $variable boş değilse burası çalışır
+}
+
+---------------------------------
+#null kullanımı
+if (is_null($variable)) {
+    // $variable null ise burası çalışır
+} else {
+    // $variable null değilse burası çalışır
+}
+
+---------------------------------
+#is_array kullanımı
+if (is_array($variable)) {
+    // $variable bir dizi ise burası çalışır
+} else {
+    // $variable bir dizi değilse burası çalışır
+}
+
+----------------------------------
+#is_string()
+if (is_string($variable)) {
+    // $variable bir dizge ise burası çalışır
+} else {
+    // $variable bir dizge değilse burası çalışır
+}
+
+----------------------------------
+#is_numeric()
+if (is_numeric($variable)) {
+    // $variable sayısal bir değer ise burası çalışır
+} else {
+    // $variable sayısal bir değilse burası çalışır
+}
+
+----------------------------------
+#is_bool()
+if (is_bool($variable)) {
+    // $variable boolean bir değer ise burası çalışır
+} else {
+    // $variable boolean bir değilse burası çalışır
+}
+
