6e696faf85a374b582222f71b42860a597b3e969
Muslum <48261594+muslumhack@users.noreply.github.com>
2024-03-11 23:16:07 +0300

    Update macchanger.py

--- python/macchanger.py
+++ python/macchanger.py
@@ -1,5 +1,6 @@
 import subprocess
 import argparse
+import re
 
 def get_arguments():
     parser = argparse.ArgumentParser()
@@ -18,5 +19,26 @@ def macchanger(interface, new_mac):
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
+def get_new_mac_address(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig",interface])
+    output_regex = re.search(rb"\s([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})",ifconfig_result)
+    
+    if output_regex:
+        output_regex = output_regex.group(0).decode('utf-8')
+        return "[+] New MAC Address ->" + output_regex
+    else:
+        return "Not found mac address" 
+def get_past_mac_address(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig",interface])
+    output_regex = re.search(rb"\s([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})",ifconfig_result)
+    
+    if output_regex:
+        output_regex = output_regex.group(0).decode('utf-8')
+        return "[+] Past MAC Address ->" + output_regex
+    else:
+        return "Not found mac address" 
 options = get_arguments()
+print(get_past_mac_address(options.interface))
 macchanger(options.interface, options.new_mac)
+print(get_new_mac_address(options.interface))
+
