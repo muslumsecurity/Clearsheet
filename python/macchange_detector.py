5373c3b331f04a71d5020b1b4d3aac4d3672f9c6
Muslum <48261594+muslumhack@users.noreply.github.com>
2024-03-12 19:54:00 +0300

    Update macchange_detector.py

--- python/macchange_detector.py
+++ python/macchange_detector.py
@@ -7,21 +7,21 @@ import subprocess
 
 def get_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--interface", dest="interface", help="MAC adresini değiştirmek için arayüz")
+    parser.add_argument("-i", "--interface", dest="interface", help="MAC adresini değiştirmek için arayüzü giriniz.")
+    parser.add_argument("-m","--mac",dest="mac address",help="Lütfen denetlenmesi gereken mac adresini gir.")
     
     options = parser.parse_args()
     if not options.interface:
         parser.error('Please check interface')
     return options
 
-def scan_mac(interface):
-    mac_address = "00:11:22:33:44:56"
+def scan_mac(interface,mac):
     ifconfig_result = subprocess.check_output(["ifconfig",interface])
     output_regex = re.search(r"ether\s([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})", str(ifconfig_result))
     
     if output_regex:
         current_mac = output_regex.group(0).split()[1]
-        if current_mac == mac_address:
+        if current_mac == mac:
             print("Mac adresi değiştirilmemiş.")
         else:
             print("Mac adresi değişmiş.")
@@ -29,8 +29,7 @@ def scan_mac(interface):
         print("Mac adresi bulunamadı.")
 
 options = get_arguments()
-
-schedule.every(5).seconds.do(scan_mac, interface=options.interface)
+schedule.every(5).seconds.do(scan_mac, interface=options.interface,mac=options.mac)
 
 while True:
     schedule.run_pending()
