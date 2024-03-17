3d4114dee4a7c6049117b61f87b2789848d9b5f9
Muslum <48261594+muslumhack@users.noreply.github.com>
2024-03-17 15:21:37 +0300

    Update sniff.py

--- python/sniff.py
+++ python/sniff.py
@@ -1,26 +1,36 @@
 import scapy.all as scapy
 from scapy.layers import http
 
-# dir_list'i burada tanımlayın
+# Define dir_list here if needed
 
 
 def sniff(interface):
     scapy.sniff(iface=interface, prn=process_sniffed_packet, store=False)
 
+
 def get_url(packet):
-    url = packet[http.HTTPRequest].Host.decode() +  packet[http.HTTPRequest].Path.decode()
-    return url   
-    
-    
+    url = packet[http.HTTPRequest].Host.decode() + packet[http.HTTPRequest].Path.decode()
+    return url
+
+
+def ext_data(packet):
+    if packet.haslayer(scapy.Raw):
+        load = packet[scapy.Raw].load.decode()
+        return load
+   
+
+
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
-       url = get_url(packet)
-        if packet.haslayer(scapy.Raw):
-            load = packet[scapy.Raw].load.decode()
-            print("[+] HTTP host:" + url)
-            print("------------- DATA -------------------")
-            print(load)
-            print("--------------------------------------")
-    
-                
+        url = get_url(packet)
+        print("[+] HTTP host:" + url)
+        print("------------- DATA -------------------")
+        data = ext_data(packet)
+        if data:
+            print('[+] Data ->' + str(data))
+        print("-----------------------------------")
+	    
+
+
+
 sniff('eth0')
