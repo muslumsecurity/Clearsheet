28ab15ae02d426c057e5de3025e181dba880914c
Muslum <48261594+muslumsecurity@users.noreply.github.com>
2024-03-23 12:16:11 +0300

    Update and rename get to get.php

---
+++ security_teori/HttpMethods/get.php
@@ -0,0 +1,33 @@
+<?php
+
+class HttpMethodService{
+        public function getMethod($requestData){  
+        }
+        public function postMethod($requestData){
+        }
+        public function deleteMethod($requestData){
+        }
+        public function putMethod($requestData){
+        }
+        public function patchMethod($requestData){
+        }
+}
+
+class HttpStatusCodeService{
+       public function informationalMethod(){
+       }
+       public function successMethod(){
+       }
+       public function redirectionMethod(){
+       }
+       public function clientErrorMethod(){
+       }
+       public function serverErrorMethod(){
+       }
+  
+       
+}
+
+
+
+?>
