854b43c221cd6043b93b6ed0d72205b18b5f253e
Muslum <48261594+muslumsecurity@users.noreply.github.com>
2024-03-28 00:55:14 +0300

    Update basic-auth.php

--- security_teori/AuthMethod/basic-auth.php
+++ security_teori/AuthMethod/basic-auth.php
@@ -64,9 +64,9 @@ class AuthenticationManager{
 // Kullanıcı adı ve parolanın doğruluğunu kontrol etme
 if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW']){
     $AuthenticationManager = new AuthenticationManager;
-    echo $AuthenticationManager->authLogin($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']);
     header('WWW-Authenticate: Basic realm="Giriş yapınız"');
     header('HTTP/1.0 401 Unauthorized');
+    echo $AuthenticationManager->authLogin($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']);
 }
 
 
