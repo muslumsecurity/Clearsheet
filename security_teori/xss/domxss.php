5b60190b09153e4507df133f38a2586f853a3fd7
Muslum <48261594+muslumsecurity@users.noreply.github.com>
2024-04-06 13:27:11 +0300

    Update domxss.php

--- security_teori/xss/domxss.php
+++ security_teori/xss/domxss.php
@@ -3,7 +3,7 @@ header("X-XSS-Protection:0");
 if($_SERVER['REQUEST_METHOD'] == "GET"){
   $name = $_GET['name'];
   echo "Merhaba $name<br/>";
-  echo "<a id='link' href=""http://www.google.com">Indırme Lınkı</a>';
+  echo "<a id='link' href=""http://www.google.com">Indırme Lınkı</a>";
 }
 
 
