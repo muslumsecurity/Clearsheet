09e9c1573d82a6d10e6c5a6dfc8c3362e67ff6c3
Muslum <48261594+muslumsecurity@users.noreply.github.com>
2024-04-09 02:03:52 +0300

    Update xssstored.php

--- security_teori/xss/xssstored.php
+++ security_teori/xss/xssstored.php
@@ -1,27 +1,41 @@
 <?php
 
 class DatabaseManagement{
-  private $host = "" ;
-  private $dbname = "" ;
-  private $dbpassword = "" ;
-  private $dbusername = "" ;
+    private $host = "localhost";
+    private $dbname = "hactivist";
+    private $dbusername = "root";
+    private $dbpassword = "";
 
-  private function connectToDatabase(){
-   $pdo = new PDO('mysql:host=$this->host;dbname=$this->dbname', '$this->dbusername', '$this->dbpassword');
-   return $pdo;
-  }
+    private function connectToDatabase() {
+        $dsn = "mysql:host={$this->host};dbname={$this->dbname}";
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ];
+        return new PDO($dsn, $this->dbusername, $this->dbpassword, $options);
+    }
 
-  public function listsDatabase(){
-      $pdo = parents::connectToDatabase;
-      $sql = "SELECT * FROM tablo_adi where name= $userSearchQuery";
-      $response = [];
-      foreach ($pdo->query($sql) as $row) {
-          $response[] =  $row;
-      }
-    
-      return $response;
-  }
+    public function listsDatabase() {
+        $pdo = $this->connectToDatabase();
+        // Basit bir temizleme işlemi (tam bir güvenlik sağlamaz)
+        
+        
+        $sql = "SELECT * FROM users";
+        $response = [];
+        foreach ($pdo->query($sql) as $row) {
+            $response[] = $row;
+        }
 
+        return $response;
+    }
+
+    public function userInsert($userName,$userPassword,$userRole){
+        $pdo = $this->connectToDatabase();
+        $sql = "INSERT INTO users (username, password , role ) VALUES ('$userName', '$userPassword', '$userRole')";
+        $pdo->query($sql);
+
+    }
+}
 
 class StringManagement{
   public function userInputSanitazion(){
@@ -39,18 +53,37 @@ class StringManagement{
 <html>
 <head></head>
   <body>
-    <forma action="" method="post">
-      <input type="text" name="userData">
-      <input type="submit" value="Listele">
-    ---------------------------------------------------------------
+    <form action="" method="post">
+      <input type="text" name="userName" placeholder="Username">
+      <input type="text" name="userPassword" placeholder="Password">
+      <input type="text" name="userRole" placeholder="User Role">
+      <input type="submit" value="ekle"><br/>
+      </form>
+    <pre>---------------------------------------------------------------</pre>
     <?php 
       $databaseManagement = new DatabaseManagement;
-      $userData = $_POST['userData'];
-      echo $databaseManagement->listsDatabase($userData);
-        
+     
+
+      if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $userName = $_POST['userName'];
+        $userPassword = $_POST['userPassword'];
+        $userRole = $_POST['userRole'];
+  
+        $databaseManagement->userInsert($userName,$userPassword,$userRole);
+      }
+      
+
+      $json = $databaseManagement->listsDatabase();
+      foreach ($json as $key) {
+        echo "İd : " . $key['id']."<br/>";
+        echo "Username : ". $key['username']."<br/>";
+        echo "Password : " . $key['password']."<br/>";
+        echo "Role : " . $key['role']."<br/>";
+        echo "<br/>";
+      }        
     ?>
 
   
-    </form>
+   
   </body>
 </html>
