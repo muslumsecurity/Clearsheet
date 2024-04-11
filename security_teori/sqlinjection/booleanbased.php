bce7252555e81b5170f55048b9e4fa8e7553fc99
Muslum <48261594+muslumsecurity@users.noreply.github.com>
2024-04-11 15:23:18 +0300

    Create booleanbased.php

---
+++ security_teori/sqlinjection/booleanbased.php
@@ -0,0 +1,66 @@
+<?php
+
+class DatabaseManagement{
+    private $host = "localhost";
+    private $dbname = "hactivist";
+    private $dbusername = "root";
+    private $dbpassword = "";
+
+    private function connectToDatabase() {
+        $dsn = "mysql:host={$this->host};dbname={$this->dbname}";
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ];
+        return new PDO($dsn, $this->dbusername, $this->dbpassword, $options);
+    }
+
+    public function listsDatabase($userSearchQuery) {
+        $pdo = $this->connectToDatabase();
+        // Basit bir temizleme işlemi (tam bir güvenlik sağlamaz)
+        
+        
+        $sql = "SELECT * FROM users WHERE username = '$userSearchQuery'";
+        $stmt = $pdo->query($sql);
+        if ($stmt->rowCount() > 0) {
+          echo 'true';
+        }else {
+          echo 'false';
+        }
+        
+    }
+}
+
+class StringManagement{
+  public function userInputSanitazion(){
+  }
+
+  public function checkInputVeriable(){
+  }
+}
+
+
+?>
+
+
+
+<html>
+<head></head>
+  <body>
+    <form action="" method="post">
+      <input type="text" name="userData">
+      <input type="submit" value="Listele"><br/>
+      </form>
+    <pre>---------------------------------------------------------------</pre>
+    <?php 
+      $databaseManagement = new DatabaseManagement;
+      $userData = $_POST['userData'];
+      $response = $databaseManagement->listsDatabase($userData);
+      echo $response;
+     
+    ?>
+
+  
+   
+  </body>
+</html>
